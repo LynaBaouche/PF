815ad6e4ebfb618f89f5b55bfe4e002935401727
Lynabch <42008865@parisnanterre.fr>
2025-11-01 16:09:53 +0100

    TD3

--- PF-Java/build.gradle.kts
+++ PF-Java/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("java")
+    id("application")
+}
+application {
+    mainClass.set("org.example")
 }
 
 group = "org.example"
@@ -14,6 +18,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("io.vavr:vavr:0.10.4")
+    implementation("org.apache.logging.log4j:log4j-api:2.19.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.1")
+
 }
 
 tasks.test {
